faaf5fe0a8f6b07c8655ea59cc5775521a7a6544
Amar Jyoti Patra <amarjyotipatra511@gmail.com>
2023-01-08 23:44:51 +0530

    1st commit

---
+++ src/App.js
@@ -0,0 +1,188 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="nav-bar">
+        <div className="logo">
+          <img src="./Assets/Group 114.png" alt="Varal Logo" />
+        </div>
+        <div className="hamburger"></div>
+        <div className="empty-space"></div>
+        <div className="options">
+          <div className="nav-options">Home</div>
+          <div className="nav-options">Services</div>
+          <div className="nav-options">Pricing</div>
+          <div className="nav-options">About Us</div>
+          <div className="nav-options buttonType">Start A Company</div>
+        </div>
+      </div>
+      <div className="inputBox">
+        <input className="inputBox2" placeholder="  Search a Term | Topic" />
+      </div>
+      <div className="header1 secondary-head">Claim a free Quote</div>
+      <div className="header primary-head">
+        <h1>
+          <span>
+            <u>Get started </u>
+          </span>
+          on fulfilling
+          <br /> your Dubai or UAE <br />
+          dream.
+        </h1>
+        <p className="header1 secondary-head">UAE & offshore Company</p>
+        <h3 className="text1 secondary-head">
+          We provide you with information about UAE or <br /> Offshore Company
+          Registration & help you <br /> setup your company with a bank account
+          and
+          <br /> visas.
+        </h3>
+        <p>
+          <span className="text2 secondary-heAad">Start a Company</span>
+          <span className="text3 secondary-head">Renew a Company</span>
+        </p>
+      </div>
+      <div className="header3">
+        <img src="./Assets/ILLUSTRATION.png" alt="Varal Header Image" />
+      </div>
+      <div className="text4 secondary-head">
+        Watch the video about UAE or Offshore Company Registration
+        <img src="./Assets/Group 117.png" alt="Video Icon" />
+      </div>
+      <div className="mid-body primary-head">
+        <h1>
+          <u>Dedicated</u> <br />
+          to our mission we are.
+        </h1>
+      </div>
+      <div className="box">
+        <div className="box1">
+          <img src="./Assets/dungeon.png" alt="Icon" />
+          <h3>UAE Free Zone Company</h3>
+          <p className="text5">
+            Your registration agent, will answer all of your questions and help
+            you to reach a conclusion that meets your objectives of investing in
+            the UAE.
+          </p>
+          <p className="text6">Get Started</p>
+        </div>
+        <div className="box1">
+          <img src="./Assets/dungeon.png" alt="Icon" />
+          <h3>Dubai Local Companies</h3>
+          <p className="text5">
+            Your registration agent, will answer all of your questions and help
+            you to reach a conclusion that meets your objectives of investing in
+            the UAE.
+          </p>
+          <p className="text6">Get Started</p>
+        </div>
+        <div className="box1">
+          <img src="./Assets/dungeon.png" alt="Icon" />
+          <h3>Offshore Companies</h3>
+          <p className="text5">
+            Your registration agent, will answer all of your questions and help
+            you to reach a conclusion that meets your objectives of investing in
+            the UAE.
+          </p>
+          <p className="text6">Get Started</p>
+        </div>
+      </div>
+      <div className="rowCard">
+        <div className="card">
+          <h3>Bank Account Setup</h3>
+          <p className="text5">
+            Your registration agent, will answer all of your questions and help
+            you to reach a conclusion that meets your objectives of investing in
+            the UAE.
+          </p>
+          <p className="card-text">Learn More -{">"}</p>
+        </div>
+        <div className="card">
+          <h3>UAE Company Visas</h3>
+          <p className="text5">
+            Your application for visas is critical especially if you intend to
+            move to Dubai. This becomes even more urgent if your family will
+            also move with you. All the paperwork is done on your behalf
+            smoothly so that you may only focus on doing what matters most to
+            you.
+          </p>
+          <p className="card-text">Learn More -{">"}</p>
+        </div>
+      </div>
+      <div className="rowCard">
+        <div className="card">
+          <h3>Advice & Guidance</h3>
+          <p className="text5">
+            All activities in the UAE are licensed. Whether manufacturing,
+            finance, trading, marketing, consultancy or restaurants. In some
+            countries only manufacturing is licensed. In others there is a
+            threshold below which business are encouraged. Get our insightfull
+            guidance today.
+          </p>
+          <p className="card-text">Learn More -{">"}</p>
+        </div>
+        <div className="card">
+          <h3>Registration Document Perparation</h3>
+          <p className="text5">
+            Several documents must be prepared to start the process of
+            registering a new company in the United Arab Emirates. Be it a Dubai
+            local company, a free zone one or an offshore entity. Your
+            registered agent is dedicated to get this done for you for a
+            seamless process.
+          </p>
+          <p className="card-text">Learn More -{">"}</p>
+        </div>
+      </div>
+      <div className="tab">
+        <div className="tab1">
+          <h4>AJMAN OFFSHORE</h4>
+          <h1>$2,177</h1>
+          <span className="text7">One time Payment</span>
+          <p>Ajman Offshore New Company formation includes</p>
+          <p>Attestation Charges</p>
+          <p>Registered Agent</p>
+          <p>Varal Administrative</p>
+          <p>Preparing Statutory File</p>
+          <p>Liasing with Registration Authorities</p>
+          <p>Due Diligence Appraisal</p>
+          <p>Seal Charges</p>
+          <p>Certificate of Good Standing</p>
+          <p className="btn">Continue</p>
+        </div>
+        <div className="tab2">
+          <h4>AJMAN OFFSHORE</h4>
+          <h1>$2,177</h1>
+          <span>One time Payment</span>
+          <p>Ajman Offshore New Company formation includes</p>
+          <p>Attestation Charges</p>
+          <p>Registered Agent</p>
+          <p>Varal Administrative</p>
+          <p>Preparing Statutory File</p>
+          <p>Liasing with Registration Authorities</p>
+          <p>Due Diligence Appraisal</p>
+          <p>Seal Charges</p>
+          <p>Certificate of Good Standing</p>
+          <p className="btn2">Continue</p>
+        </div>
+        <div className="tab1">
+          <h4>AJMAN OFFSHORE</h4>
+          <h1>$2,177</h1>
+          <span className="text7">One time Payment</span>
+          <p>Ajman Offshore New Company formation includes</p>
+          <p>Attestation Charges</p>
+          <p>Registered Agent</p>
+          <p>Varal Administrative</p>
+          <p>Preparing Statutory File</p>
+          <p>Liasing with Registration Authorities</p>
+          <p>Due Diligence Appraisal</p>
+          <p>Seal Charges</p>
+          <p>Certificate of Good Standing</p>
+          <p className="btn">Continue</p>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
